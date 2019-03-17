30ecd17a82bb3e285030bb1640e9f9d8a1d6bbd0
paulnguyen <paul.nguyen@sjsu.edu>
2019-03-17 15:08:29 -0700

    added fields

--- java7/reflection/class-structure/GumballMachine.java
+++ java7/reflection/class-structure/GumballMachine.java
@@ -3,9 +3,16 @@ import java.lang.reflect.* ;
 
 public class GumballMachine {
 
-	protected int cnt = 0; // inventory
-	protected int coinValue = 0 ; // cents
-	protected int cost = 0 ; // configure at setup
+	protected 		int cnt = 0; // inventory
+	protected 		int coinValue = 0 ; // cents
+	protected 		int cost = 0 ; // configure at setup
+
+	// unused -- for reflection api test
+ 
+	private 		int test1 = 0 ;
+	public 			int test2 = 0 ;
+	private static 	String test3 = "" ;
+
 
 	public GumballMachine(int count) {
 		this.cnt = count;
@@ -58,18 +65,41 @@ public class GumballMachine {
 	}
 	
 	// Main Class - Dump Metadata
+
 	public static void main( String args[ ] )
 	{
 		System.out.println( "***** Class Bytecode Dump *****" ) ;
 		GumballMachine m = new GumballMachine(10) ;
 		Class gmClass = m.getClass() ;
-		Method gmMethods[] = gmClass.getMethods() ;
-		for ( int i=0; i <gmMethods.length; i++ )
-		{
-			Method theMethod = gmMethods[i] ;
-			String method = theMethod.toString() ;
-			System.out.println( method ) ;
-		}
+
+        System.out.println( "\nFIELDS:" ) ;
+        Field[] fields = gmClass.getDeclaredFields();
+        for (Field f : fields) {
+
+        	int mods = f.getModifiers();
+ 
+        	if ( Modifier.isPublic(mods) )
+        		System.out.format( "  public" ) ;
+        	else if ( Modifier.isPrivate(mods) )
+        		System.out.format( "  private" ) ;
+        	else if ( Modifier.isProtected(mods) )
+        		System.out.format( "  protected" ) ;
+ 
+        	if ( Modifier.isStatic(mods) )
+        		System.out.format( " static") ;
+
+        	System.out.format( " %s %s\n", f.getType(), f.getName() ) ;
+
+        }
+
+        System.out.println( "\nMETHODS:" ) ;
+        Method gmMethods[] = gmClass.getMethods() ;
+        for ( int i=0; i <gmMethods.length; i++ )
+        {
+            Method theMethod = gmMethods[i] ;
+            String method = theMethod.toString() ;
+            System.out.format( "  %s\n", method ) ;
+        }
 		
 	}
 	
